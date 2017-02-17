fa803faccb85bfc24628b0f86aa9aa9e29946fb1
Alexander Scheuermann <alex@tnix.de>
2017-02-17 23:17:31 +0100

    nerd: generalize 60% KEYMAP and add HHKB layout
    KEYMAP_60 supports all layouts on NerD 60 V2.10

--- keyboard/nerd/keymap_60_hhkb.c
+++ keyboard/nerd/keymap_60_hhkb.c
@@ -5,14 +5,14 @@
 
 const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = KEYMAP_60(
-         ESC,   1,   2,   3,   4,   5,   6,   7,   8,   9,   0,MINS, EQL,  NO, BSPC, \
-         TAB,   Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,LBRC,RBRC,NUHS, \
+         ESC,   1,   2,   3,   4,   5,   6,   7,   8,   9,   0,MINS, EQL,BSLS, GRV, \
+         TAB,   Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,LBRC,RBRC,BSPC, \
         CAPS,   A,   S,   D,   F,   G,   H,   J,   K,   L,SCLN,QUOT,      ENT, \
-        LSFT,NUBS,   Z,   X,   C,   V,   B,   N,   M,COMM, DOT,SLSH,RSFT, FN0, \
+        LSFT,  NO,   Z,   X,   C,   V,   B,   N,   M,COMM, DOT,SLSH,RSFT, FN0, \
         LCTL,LGUI,LALT,           SPC,                      NO,RALT,RGUI,RCTL),
     [1] = KEYMAP_60(
-         GRV,  F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9, F10, F11, F12,  NO, DEL, \
-        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,PSCR,SLCK,PAUS,  UP, INS,TRNS, \
+         PWR,  F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9, F10, F11, F12, INS, DEL, \
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,PSCR,SLCK,PAUS,  UP,TRNS,TRNS, \
          FN1,VOLD,VOLU,MUTE,EJCT,TRNS,TRNS,TRNS,HOME,PGUP,LEFT,RGHT,     PENT, \
         TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,END ,PGDN,DOWN,TRNS,TRNS, \
         TRNS,TRNS,TRNS,           FN2,                    TRNS,TRNS,TRNS,TRNS),
